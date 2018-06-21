c527444677b341061276e7d8e652b3db60c6c5fa
tjian <tjian89@sina.com>
2018-06-21 22:57:40 +0800

    6666

--- MyTest/src/com/tjian/MyTest.java
+++ MyTest/src/com/tjian/MyTest.java
@@ -5,7 +5,8 @@ public class MyTest {
 		System.out.println("1111111111");
 		System.out.println("2222222222");
 		System.out.println("3333333333");
-		System.out.println("5555555555");
 		System.out.println("4444444444");
+		System.out.println("5555555555");
+		System.out.println("6666666666");
 	}
 }
